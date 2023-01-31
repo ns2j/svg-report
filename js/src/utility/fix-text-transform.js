d3df8d47f518ee26018428bf4eaeab169c90fdd9
igari <igari.y@gmail.com>
2023-01-31 12:38:37 +0900

    refactor

--- js/src/utility/fix-text-transform.js
+++ js/src/utility/fix-text-transform.js
@@ -1,5 +1,4 @@
 import jQuery from 'jquery'
-import getMatrix from './matrix'
 
 const $ = jQuery
 
@@ -13,15 +12,13 @@ export default (text) => {
     dx = parseFloat(match[1])
     dy = parseFloat(match[2])
   }    
-console.log(`dx: ${dx}`)
-console.log(`dy: ${dy}`)
+  //console.log(`dx: ${dx}`)
+  //console.log(`dy: ${dy}`)
     
-//  const mmppx = 0.26458333 //mm per px (inkscape use 96dpi)
-
   let x = parseFloat(text.attr("x"))
   let y = parseFloat(text.attr("y"))
-  text.attr("transform", `matrix(1, 0, 0, 1, ${x + dx}, ${y + dy})`)
-  text.attr("x", 0)
-  text.attr("y", 0)
-
+  //text.attr("transform", `matrix(1, 0, 0, 1, ${x + dx}, ${y + dy})`)text.attr("transform", `matrix(1, 0, 0, 1, ${x + dx}, ${y + dy})`)
+  text.attr('transform', `translate(${x + dx}, ${y + dy})`)
+  text.attr('x', 0)
+  text.attr('y', 0)
 }
--- js/src/utility/split-string-by-width.js
+++ js/src/utility/split-string-by-width.js
@@ -1,4 +1,21 @@
-import subStringByWidth from './sub-string-by-width'
+import stringWidth from 'string-width'
+
+function subStringByWidth(string, start, width) {
+  let currentWidth = 0
+  let subString = ''
+
+  for (let i = start; ; i++) {
+    const char = string.substr(i, 1)
+    currentWidth += stringWidth(char)
+    if (currentWidth <= width && i <= string.length) {
+      subString += char
+    }
+    if (currentWidth >= width || i >= string.length) {
+      return subString
+    }
+  }
+}
+
 
 export default (string, width) => {
   let splits = []
